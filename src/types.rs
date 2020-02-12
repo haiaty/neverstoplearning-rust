601672525ca37a9d92b75bf0862d06456578f18f
haiaty <varottohaiaty@gmail.com>
2020-02-12 23:43:41 +0100

    Learning types

--- src/types.rs
+++ src/types.rs
@@ -9,14 +9,39 @@ fn main () {
     //integers
 
     // u8  - unsigned 8 bit integer
-    let eightBitUnsignedInteger : u8 = 2;
+    let eightBitUnsignedInteger : u8 = 255;
 
-    // u32 - unsigned integer - 32 bits of space
+     // u32 - unsigned integer - 32 bits of space
     let unsignedInteger : u32 = 3;
 
     //i32 - signed integer - 32 bits of space
     let signedInteger : i32 = 3;
 
+    /*
+    If you run in debug:
 
-    println!("Some integers: {} {} {}", eightBitUnsignedInteger, unsignedInteger, signedInteger)
+    thread 'main' panicked at 'attempt to add with overflow', src/types.rs:14:13
+    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace.
+    
+    If you run with --release (ex)
+    you will not have any error but your code will not work as expected
+
+    */
+    let integerOverflow = eightBitUnsignedInteger + 1;
+
+
+    println!("Some integers: {} {} {}", eightBitUnsignedInteger, unsignedInteger, signedInteger);
+
+    // Float point - are numbers with decimal points
+    // - default is f64
+
+    let defaultIsF64 = 3.0;
+
+    // f32 precision
+    let floatingPoint32: f32 = 2.0;
+
+    println!("Some floating points: {} {}", defaultIsF64, floatingPoint32)
+
+
+
 }
