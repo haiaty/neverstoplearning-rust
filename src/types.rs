376f4f944af6c1f3e4927d33fd3903c99fd26674
Haiaty Varotto <haiaty.varotto@nubilaria.com>
2020-02-12 16:42:35 +0100

    leaening

---
+++ src/types.rs
@@ -0,0 +1,22 @@
+
+// this is needed if you don't want to see warnings
+// about the names of varianles in camelCase
+#![allow(non_snake_case)]
+fn main () {
+
+    //scalar types
+
+    //integers
+
+    // u8  - unsigned 8 bit integer
+    let eightBitUnsignedInteger : u8 = 2;
+
+    // u32 - unsigned integer - 32 bits of space
+    let unsignedInteger : u32 = 3;
+
+    //i32 - signed integer - 32 bits of space
+    let signedInteger : i32 = 3;
+
+
+    println!("Some integers: {} {} {}", eightBitUnsignedInteger, unsignedInteger, signedInteger)
+}
