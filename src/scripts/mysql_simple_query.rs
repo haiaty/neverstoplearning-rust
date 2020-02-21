d2513d3d1b169f26d3a6a678126f1ad7e863c3ce
Haiaty Varotto <varottohaiaty@gmail.com>
2020-02-21 16:51:34 +0100

    Create mysql_simple_query.rs

---
+++ src/scripts/mysql_simple_query.rs
@@ -0,0 +1,34 @@
+
+use mysql::params;
+
+#[derive(Debug, PartialEq, Eq)]
+struct MyStructureRecord {
+    always_present: Option<String>,
+    allowed_values: Option<String>
+}
+
+
+fn main() {
+
+ let pool = mysql::Pool::new("mysql://root:rootdb@localhost:3306/mydb").unwrap();
+
+    let all_records: Vec<MyStructureRecord> =
+        pool.prep_exec("
+        SELECT always_present, allowed_values
+        FROM my_schema.my_table
+        WHERE platform=:param", params! {
+            "param" => "A PARAM",
+        })
+ 
+            .map(|result| {
+                result.map(|x| x.unwrap()).map(|row| {
+                    let (always_present, allowed_values) = mysql::from_row(row);
+
+                    MyStructureRecord {
+                        always_present,
+                        allowed_values
+                    }
+                }).collect()
+            }).unwrap(); // Unwrap `Vec<MyStructureRecord>`
+
+}
