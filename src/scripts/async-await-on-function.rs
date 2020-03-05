5ef4400d2157d28ee6943190e854e93fec8ee298
Haiaty Varotto <varottohaiaty@gmail.com>
2020-03-05 17:31:31 +0100

    Create async-await-on-function.rs

---
+++ src/scripts/async-await-on-function.rs
@@ -0,0 +1,46 @@
+use std::error::Error;
+/*
+
+Note: we return Result<(), Box<dyn Error> if we have different kind of errors
+while, if we know that the result would be of a given type we could use for example : Result<(), reqwest::Error>. 
+
+This is because of propagating error . see:https://doc.rust-lang.org/book/ch09-02-recoverable-errors-with-result.html
+
+
+NOTE: since we use await we should declare the function async
+*/
+pub async fn create_new_field() -> Result<(), Box<dyn Error>> {
+
+
+    let client = reqwest::Client::new();
+
+
+    let res = client.post("http://tools.francescozanoni.it/http/request.php")
+        .body("the exact body that is sent")
+        .send()
+        .await?; // the ? means that if there is any error, it will be propagated
+
+    println!("res status: {:#?}", res.status());
+    println!("res status: {:#?}", res.text().await?);
+
+
+    Ok(()) // since this return void, we should wrap void type () into an OK enum
+
+
+}
+
+/*
+
+the main of code above
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+
+   create_new_field().await;
+
+   Ok(())
+
+}
+
+
+*/
