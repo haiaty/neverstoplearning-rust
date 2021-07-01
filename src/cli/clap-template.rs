80ee1a6b20dce5c3e04102166412f500e8ad5332
Haiaty Varotto <haiaty.varotto@nubilaria.com>
2021-07-01 14:54:55 +0200

    Added clap template for Cli programs

--- src/cli/clap-template.rs
+++ src/cli/clap-template.rs
@@ -9,11 +9,21 @@ fn main() {
         .version("1.0")
         .author("haiaty")
         .about("will be displayed when you run --help on the binary produces")
+        // this is how to set input arguments
+        // ex usage:    ./target/debug/myprogram "FIRSTARG"
         .arg(Arg::new("FIRSTARG") //the name of the argument is needed in order to take it later
                  .about("the first argument passed on the command line")
                  .required(true)
                  .index(1) //indicates that this is the firs argument
         )
+        // this is how to set custom options
+        //--source_type <SOURCETYPE>    Sets a custom config file
+        // ex usage:    ./target/debug/myprogram --source_type=TYPE
+        .arg(Arg::new("source_type")
+            .long("source_type")
+            .value_name("SOURCETYPE")
+            .about("Sets a custom config file")
+            .takes_value(true)) //Specifies that the argument takes a value at run time.
         .get_matches();
 
     if let Some(i) = matches.value_of("FIRSTARG") {
--- src/libraries.md
+++ src/libraries.md
@@ -4,3 +4,10 @@ Command line parsing arguments
 ```
 clap="2.33.1"
 ```
+
+Error handling
+
+```
+anyhow
+thiserror
+```
